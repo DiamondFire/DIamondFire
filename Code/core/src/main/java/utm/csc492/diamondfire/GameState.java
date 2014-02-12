e2ddf8cc78493bc0e63a9f6c82cb4a6b7b0104ba
Yasith Vidanaarachchi <yasith@tuxv.net>
2014-02-12 01:47:20 -0500

    Add a GameState object, which loads some common things.

---
+++ Code/core/src/main/java/utm/csc492/diamondfire/GameState.java
@@ -0,0 +1,36 @@
+package utm.csc492.diamondfire;
+
+/**
+ * Created by yasith on 2/12/2014.
+ */
+public class GameState {
+
+    private static GameState instance = null;
+    private DiamondFire game;
+
+    // Reason for not keeping this private:
+    // This will be used a lot for loading sprites, throughout the game
+    // having a getter would just make the code longer without any benefit.
+    public TextureAtlas atlas;
+
+    // Private constructor prevents initialization from outside
+    private GameState() {
+        atlas = new TextureAtlas(Gdx.files.internal("images/pack"));
+    }
+
+    public static synchronized GameState getInstance() {
+        if(instance == null) {
+            instance = new GameState();
+        }
+
+        return instance;
+    }
+
+    public DiamondFire getGame() {
+        return game;
+    }
+
+    public void setGame(DiamondFire game) {
+        this.game = game;
+    }
+}
